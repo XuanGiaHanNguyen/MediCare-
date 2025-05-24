d83b858a0bf39bc2b2be80bd4837934b1919e5b5
XuanGiaHanNguyen <han316@usf.edu>
2025-05-24 16:47:50 +0700

    finished the header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 import Landing from "./pages/Landing"
+import Header from "./component/header"
 
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Landing/>}/>
       </Routes>
---
+++ frontend/src/component/header.jsx
@@ -0,0 +1,31 @@
+// make header a prop, not an official site (arrow function)
+import { Link } from "react-router-dom"
+
+
+const Header = (props) => {
+    return(
+        <div className="py-3 px-10 flex flex-row justify-between bg-sky-800">
+            <h1 className="font-bold text-2xl text-white">MEDICARE+</h1>
+            <div className="pt-1 text-white font-medium flex flex-row gap-8">
+                <Link className="group">
+                    Featured
+                    <div class="bg-sky-600 h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
+                </Link>
+                <Link className="group">
+                    Services
+                    <div class="bg-sky-600 h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
+                </Link>
+                <Link className="group">
+                    Blog
+                    <div class="bg-sky-600 h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
+                </Link>
+                <Link className="group">
+                    Contact Us
+                    <div class="bg-sky-600 h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
