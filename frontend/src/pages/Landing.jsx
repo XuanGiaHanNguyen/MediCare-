74055058dfa129c67d8ecf16b798563c910c8bbe
XuanGiaHanNguyen <han316@usf.edu>
2025-05-24 17:11:13 +0700

    changed layout

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,11 +2,17 @@ function Landing () {
     return(
         <div className="flex flex-col">
             <div className="w-full h-screen bg-blue-100 flex flex-row">
-                <div>
-
+                <div className="border-2 flex-[1.25] px-10 flex justify-center align-center items-center">
+                    <div>
+                        <p className="border-2 ">Trusted by 500+ Healthcare Facilities</p>
+                        <h1>Streamline Your Hospital Operations</h1>
+                        <h2>
+                            Complete hospital management solution that digitizes patient records, schedules appointments, manages staff, and optimizes healthcare delivery for better patient outcomes.
+                        </h2>
+                    </div>
                 </div>
-                <div>
-                    
+                <div className="border-2 flex-1">
+                        1
                 </div>
             </div>
         </div>
