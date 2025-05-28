7b3408c56799611b4a79e223123fd1fc9e848f59
XuanGiaHanNguyen <han316@usf.edu>
2025-05-28 20:13:58 +0700

    moooo

--- frontend/src/component/DockHeader.jsx
+++ frontend/src/component/DockHeader.jsx
@@ -12,7 +12,7 @@ const HospitalHeader = (props) => {
       {/* Center - Search Bar */}
       <div className="flex-1 max-w-md mx-8">
         <div className="relative">
-          <Search className="absolute left-2 top-1/5 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
+          <Search className="absolute left-2 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
           <input
             type="search"
             placeholder="Search"
