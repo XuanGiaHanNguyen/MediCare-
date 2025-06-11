9558b85edda1646b706164e2d881ddbf76e7751b
XuanGiaHanNguyen <han316@usf.edu>
2025-06-11 15:58:37 +0700

    connected to server

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,13 @@
 const connect = require("./connect")
+const express = require("express")
+const cors = require("cors")
 
+const app = express()
+const PORT = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT, ()=>{
+    connect.connectToServer()
+})
