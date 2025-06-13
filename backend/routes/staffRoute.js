8e5468c9a268bf16241d02f4ff2ad84fa32057f5
XuanGiaHanNguyen <han316@usf.edu>
2025-06-13 07:32:32 +0700

    staff routing setup

---
+++ backend/routes/staffRoute.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const database = require("../connect")
+const ObjectId = require("mongodb").ObjectId
+
+let staffRoute = express.Router()
+
+// Get All
+
+// Get One
+
+// Create One
+
+// Edit One
+
+// Delete One 
