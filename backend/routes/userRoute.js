68df40e50401cc012684a5a2fa9bd3e184334293
XuanGiaHanNguyen <han316@usf.edu>
2025-06-17 22:07:15 +0700

    added bcrypt

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,10 @@
 const database = require("../connect")
 const express = require("express")
 const ObjectId = require("mongodb").ObjectId
+const bcrypt = require("bcrypt")
 
 const userRoute = express.Router()
+const SALT_ROUNDS = 6
 
 // Get All
 userRoute.route("/user").get(
@@ -34,11 +36,12 @@ userRoute.route("/user/:id").get(
 userRoute.route("/user").post(
     async (request, response) => {
         let db = database.getDB()
+        let hash = await bcrypt.hash(request.body.password, SALT_ROUNDS )
         let mongoObject = {
             is_staff: request.body.is_staff, 
             full_name: request.body.full_name, 
             email: request.body.email,
-            password: request.body.password
+            password: hash
         }
         let data = await db.collection("user").insertOne(mongoObject)
         response.json(data)
@@ -49,12 +52,13 @@ userRoute.route("/user").post(
 userRoute.route("/user/:id").put(
     async (request, response) => {
         let db = database.getDB()
+        let hash = bcrypt.hash(request.body.password, SALT_ROUNDS)
         let mongoObject = {
             $set: {
                 is_staff: request.body.is_staff, 
                 full_name: request.body.full_name, 
                 email: request.body.email,
-                password: request.body.password
+                password: hash
             }
         }
         let data = await db.collection("user").updateOne({_id: new ObjectId(request.params.id)},mongoObject)
@@ -70,6 +74,8 @@ userRoute.route("/user").delete(
         response.json(data)
     }
 )
+
+// 
 
 // Export Route 
 module.exports = userRoute
